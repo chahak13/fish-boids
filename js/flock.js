a5860b1d8ee3f7f688624351c7b26be75733912c
chahak13 <chahakmehta013@gmail.com>
2018-04-07 11:29:27 +0530

    Updated file

--- js/flock.js
+++ js/flock.js
@@ -9,10 +9,10 @@ Flock.prototype.addBoid = function(boid){
 }
 
 Flock.prototype.moveBoids = function(){
-  console.log('Reached moveBoids with '+this.boids.length+ 'boids')
+  // console.log('Reached moveBoids with '+this.boids.length+ 'boids')
   if (this.boids.length != 0) {
     for (var i = 0; i < this.boids.length; i++) {
-      this.boids[i].update(i);
+      this.boids[i].update();
       this.boids[i].borders();
       this.boids[i].render();
     }
